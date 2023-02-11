b41ab309ed6bdb3f2427f6e97de6c6de720f58c5
PuaghDev <pablofeijo181@gmail.com>
2023-02-11 12:55:36 -0300

    components_1

---
+++ components_app/lib/screens/listview1_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+  const Listview1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("ListView Tipo 1"),
+      ),
+      body: const Center(
+        child: Text('Listview1Screen'),
+      ),
+    );
+  }
+}
---
+++ components_app/lib/screens/screens.dart
@@ -0,0 +1,1 @@
+export 'package:components_app/screens/listview1_screen.dart';
